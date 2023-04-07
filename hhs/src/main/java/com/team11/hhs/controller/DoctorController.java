f109e9b495693cc0b969d87b75b0167de8f81ca5
John Niesman <john-niesman@uiowa.edu>
2023-04-06 20:42:37 -0500

    new controller for doctor role

--- hhs/src/main/java/com/team11/hhs/controller/DoctorController.java
+++ hhs/src/main/java/com/team11/hhs/controller/DoctorController.java
@@ -1,11 +1,19 @@
 package com.team11.hhs.controller;
 
+import com.team11.hhs.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/doctor")
 public class DoctorController {
 
+    private UserService userService;
 
+    @GetMapping("/home")
+    public String doctorHome(Model model){
+        return "doctorHome";
+    }
 }
